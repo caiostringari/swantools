ae1859db65c55fad3a1e75d8db3225a9a7f21185
Caio <caio.stringari@gmail.com>
2015-04-16 20:33:16 -0300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='swantools',
+      version='0.1',
+      description='Swan Utilities',
+      author='Caio Stringari',
+      author_email='caio.stringari@gmail.com',
+      url='https://github.com/caiostringari/swantools',
+      packages=['swantools'],
+     )
---
+++ swantools/__init__.py
@@ -0,0 +1,9 @@
+'''
+
+A python module with useful tools for SWAN wave model
+
+'''
+
+import swantools
+
+__authors__ = ['Caio Stringari - caio.stringari@gmail.com']
